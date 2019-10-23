6854abdfa9c389b0a1504b9ce94ff82e7759bce6
Hernán Blanco <hblanco@pm.me>
2019-10-24 01:21:01 +0200

    Use of NetworkX package

--- graph/recipegraph.py
+++ graph/recipegraph.py
@@ -1,63 +1,39 @@
-from typing import Any, Dict
+from typing import Any
 
-from graph_tool import Graph, Vertex
-from graph_tool.draw.cairo_draw import graph_draw
+import networkx as nx
+from networkx.readwrite import json_graph
+import json
 
 
-class RecipeGraph(Graph):
-    '''Class building and storing a recipe graph from recipes based on the JSON Cookbase Format.
+class RecipeGraph():
+    '''Class building and storing a recipe graph from recipes based on the Cookbase Recipe Format.
 
-    It uses the `graph-tool` module, inheriting the class :class:`graph_tool.Graph`.
+    It makes use of the `networkx` library to generate the graph.
     
-    :ivar __data: The recipe document in JSON Cookbase Format
-    :vartype __data: dict[str, Any]
-    :ivar __process_vertices: An index of graph vertices that represent processes, stored by process identifier
-    :vartype __process_vertices: dict[str, graph_tool.Vertex]
-    :ivar __used_instruments: The set of instruments being used at a given state of the graph
-    :vartype __used_instruments: set[str]
-    :ivar __vertex_properties: An instance of :class:`graph_tool.PropertyMap` storing the ingredients, instruments and processes identifiers
-    :vartype __vertex_properties: graph_tool.PropertyMap
-    :ivar __vertex_properties_c: An instance of :class:`graph_tool.PropertyMap` storing the colors for the graph vertices
-    :vartype __vertex_properties_c: graph_tool.PropertyMap
+    :ivar _used_instruments: The set of instruments being used at a given state of the graph
+    :vartype _used_instruments: set[str]
+    :ivar _g: The set of instruments being used at a given state of the graph
+    :vartype _g: networkx.classes.digraph.DiGraph
     '''
 
-    def __init__(self,
-                 data: Dict[str, Any]):
+    def __init__(self):
         '''Constructor method'''
-        super(RecipeGraph, self).__init__()
-        self.__data = data 
-        self.__process_vertices = dict()
-        self.__used_instruments = set()
-        self.__vertex_properties = self.new_vertex_property("string")
-        self.__vertex_properties_c = self.new_vertex_property("string")
+        self._used_instruments = set()
+        self._g = nx.DiGraph()
 
     def add_foodstuff(self,
                       foodstuff_id: str,
-                      process_vertex: Vertex = None) -> Any:
+                      process_id: str = None) -> Any:
         '''Adds a foodstuff and its corresponding edges to the recipe graph
 
         :param str foodstuff_id: Foodstuff identifier
-        :param process_vertex: The vertex representing the process which the added foodstuff merges into, defaults to `None`
-        :type process_vertex: graph_tool.Vertex, optional
+        :param str process_id: Identifier for the process which the added foodstuff merges into, defaults to `None`
+        :type process_id: str, optional
         '''
-        if process_vertex == None:
-            v = self.add_vertex()
-            self.__vertex_properties[v] = foodstuff_id
-#             self.vertex_properties[v] = self.__data["preparation"][foodstuff_id]["process"]
-            if (len(self.__process_vertices) == 0):
-                self.__vertex_properties_c[v] = "#99cc00"
-            else:
-                self.__vertex_properties_c[v] = "#a40000"
-            self.__process_vertices[foodstuff_id] = v
-            return v
+        if process_id == None:
+            self._g.add_node(foodstuff_id)
         else:
-            if foodstuff_id.startswith("ing"):
-                v = self.add_vertex()
-                self.__vertex_properties[v] = foodstuff_id
-                self.__vertex_properties_c[v] = "#ff6600"
-                self.add_edge(v, process_vertex)
-            elif foodstuff_id.startswith("proc"):
-                self.add_edge(self.__process_vertices[foodstuff_id], process_vertex)
+            self._g.add_edge(foodstuff_id, process_id)
 
     def add_instrument(self,
                        instrument_id: str,
@@ -69,21 +45,16 @@ class RecipeGraph(Graph):
         :param str process_id: Process identifier
         :param bool used_after: A flag indicating whether the given instrument remains used after finishing the given process (`True`) or not (`False`), defaults to `False`
         '''
-        if instrument_id not in self.__used_instruments:
-            v = self.add_vertex()
-            self.__vertex_properties[v] = instrument_id
-            self.__vertex_properties_c[v] = "#3366ff"
-            self.add_edge(v, self.__process_vertices[process_id])
+        if instrument_id not in self._used_instruments:
+            self._g.add_edge(instrument_id + ":" + process_id, process_id)
             if used_after == True:
-                self.__used_instruments.add(instrument_id)
+                self._used_instruments.add(instrument_id)
             else:
-                self.add_edge(self.__process_vertices[process_id], v)
+                self._g.add_edge(process_id, instrument_id + ":" + process_id)
+                
         elif used_after == False:
-            v = self.add_vertex()
-            self.__vertex_properties[v] = instrument_id
-            self.__vertex_properties_c[v] = "#3366ff"
-            self.add_edge(self.__process_vertices[process_id], v)
-            self.__used_instruments.remove(instrument_id)
+            self._g.add_edge(process_id, instrument_id + ":" + process_id)
+            self._used_instruments.remove(instrument_id)
 
     def output(self,
                output_path: str) -> None:
@@ -91,7 +62,9 @@ class RecipeGraph(Graph):
 
         :param str output_path: Path where output files are to be located
         '''
-        self.vertex_properties["id"] = self.__vertex_properties
-        self.vertex_properties["fill_color"] = self.__vertex_properties_c
-#         self.save(output_path + "/recipe.xml", "graphml")
-        graph_draw(self, vertex_text=self.vertex_properties["id"], vertex_fill_color=self.vertex_properties["fill_color"], vertex_font_size=11, vertex_size=20, output=output_path + "/recipe.svg", output_size=(2200,1500))
+#         self.g.add_node("ing1", api="LINK")
+#         nx.draw(self.g, pos=nx.spring_layout(self.g))
+#         nx.write_graphml(self.g, output_path + "/recipe.xml", prettyprint=True)
+#         plt.show()
+        with open(output_path + "/recipe.json", "w") as f:
+            f.write(json.dumps(json_graph.node_link_data(self._g), indent = 2))
