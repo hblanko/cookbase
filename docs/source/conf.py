2432b9e4769866d613b0ab90e6d344b798512b6f
Hernán Blanco <hblanco@pm.me>
2020-02-06 14:49:38 +0100

    Improving documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,5 +57,5 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 master_doc = 'index'
-plantuml = 'java -jar ../../plantuml.jar'
+plantuml = 'java -jar ../plantuml.jar'
 plantuml_output_format = 'svg'
