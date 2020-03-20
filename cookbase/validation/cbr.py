7e2fb3f5d9f91fc10d9f42507a5895a139db39b5
hblanko <hblanco@pm.me>
2020-03-20 01:48:02 +0100

    Renamed arguments in cookbase.validation.cbr.Validator

--- cookbase/validation/cbr.py
+++ cookbase/validation/cbr.py
@@ -39,48 +39,48 @@ class Validator():
         self.schema: Dict[str, Any] = r.json()
         self.graph: RecipeGraph = RecipeGraph()
 
-    def _store(self, data: Dict[str, Any]) -> Union[int, bool]:
+    def _store(self, cbr: Dict[str, Any]) -> Union[int, bool]:
         '''Stores a :ref:`CBR <cbr>` and its :doc:`CBRGraph <cbrg>` into database.
 
         The data is stored using the :data:`cookbase.db.handler.db_handler` object.
 
-        :param data: The dictionary containing the validated :ref:`CBR <cbr>`
-        :type data: dict[str, Any]
+        :param cbr: The dictionary containing the validated :ref:`CBR <cbr>`
+        :type cbr: dict[str, Any]
         :return: If :ref:`CBR <cbr>` and :doc:`CBRGraph <cbrg>` insertion was successful
           returns the identifier of the stored object, returns :const:`False` otherwise
         :rtype: int or bool
         '''
-        return handler.get_handler().insert_cbr(data,
+        return handler.get_handler().insert_cbr(cbr,
                                                 self.graph.get_serializable_graph())
 
-    def apply_validation_rules(self, data: Dict[str, Any]) -> Dict[str, bool]:
+    def apply_validation_rules(self, cbr: Dict[str, Any]) -> Dict[str, bool]:
         '''Validates a :ref:`CBR <cbr>` against the set of definition rules.
 
         Validation rules defined in :mod:`cookbase.validation.rules` are applied
         sequentially to ensure that the recipe document satisfies the :ref:`CBR <cbr>`
         definition.
 
-        :param data: The :ref:`CBR <cbr>` to be validated
-        :type data: dict[str, Any]
+        :param cbr: The :ref:`CBR <cbr>` to be validated
+        :type cbr: dict[str, Any]
         :return: A dictionary indicating the results of the validation process
         :rtype: dict[str, bool]
         '''
         result = {}
         rule = 'ingredients_are_valid'
         result[rule] = getattr(rules.Semantics, rule)(
-            data['ingredients']
+            cbr['ingredients']
         ).has_passed(strict=False)
         rule = 'foodstuff_and_appliance_references_are_consistent'
         result[rule] = getattr(rules.Semantics, rule)(
-            data['ingredients'],
-            data['appliances'],
-            data['preparation']
+            cbr['ingredients'],
+            cbr['appliances'],
+            cbr['preparation']
         ).has_passed(strict=False)
         rule = 'processes_and_appliances_are_valid_and_processes_requirements_met'
         result[rule] = getattr(rules.Semantics, rule)(
-            data['appliances'], data['preparation']
+            cbr['appliances'], cbr['preparation']
         ).has_passed(strict=False)
-        self.graph.build_graph(data)
+        self.graph.build_graph(cbr)
         rule = 'ingredients_used_exactly_once'
         result[rule] = getattr(rules.Graph, rule)(self.graph).has_passed(strict=False)
         rule = 'single_final_process'
@@ -90,7 +90,7 @@ class Validator():
         return result
 
     def validate(self,
-                 data: Dict[str, Any],
+                 cbr: Dict[str, Any],
                  store: bool = False) -> Dict[str, Union[bool, int]]:
         '''Main function of the class, it performs the validation of a :ref:`CBR <cbr>`
         and builds the :doc:`CBRGraph <cbrg>`.
@@ -100,8 +100,8 @@ class Validator():
         applied to ensure that the recipe document satisfies the :ref:`CBR <cbr>`
         definition.
 
-        :param data: The :ref:`CBR <cbr>` to be validated
-        :type data: dict[str, Any]
+        :param cbr: The :ref:`CBR <cbr>` to be validated
+        :type cbr: dict[str, Any]
         :param store: A flag indicating whether the validated CBR and :doc:`CBRGraph
           <cbrg>` should be stored in database, defaults to :const:`False`
         :type store: bool, optional
@@ -110,12 +110,12 @@ class Validator():
           :ref:`CBR <cbr>` under the :code:`'inserted_id'` key
         :rtype: dict[str, Union[bool, int]]
         '''
-        jsonschema.validate(data, self.schema)
+        jsonschema.validate(cbr, self.schema)
         self.graph.clear()
-        result = self.apply_validation_rules(data)
+        result = self.apply_validation_rules(cbr)
 
         if store and False not in result.values():
-            r = self._store(data)
+            r = self._store(cbr)
 
             if r:
                 result['inserted_id'] = r
